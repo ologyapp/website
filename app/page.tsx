64f3732c41036678a3f4b4914663b17f7a1e3560
Hillary Victor <chillaryvictor@gmail.com>
2026-07-27 16:58:41 +0100

    debug iphone

--- app/page.tsx
+++ app/page.tsx
@@ -3955,8 +3955,16 @@ export default function Home() {
             {/* Modal */}
             <div
               ref={cardRef}
-              className="relative flex w-[945.24px] max-w-full min-h-[107.29px] h-auto px-6 py-7 md:p-12.5 flex-col items-center gap-6 md:gap-12.5 rounded-[16.912px] border border-white/50 bg-cover bg-center bg-no-repeat bg-lightgray overflow-y-auto max-h-[90vh]"
               onClick={(e) => e.stopPropagation()}
+              className="relative flex w-[945.24px] max-w-full min-h-[107.29px] h-auto px-6 py-7 md:p-12.5 flex-col items-center gap-6 md:gap-12.5 rounded-[16.912px] border border-white/50 overflow-y-auto max-h-[90vh]"
+              style={{
+                backgroundImage: showLogoOnModal
+                  ? `url(${getCardPoster(cardType)})`
+                  : "none",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
             >
               {/* <video
                 autoPlay
@@ -3985,12 +3993,12 @@ export default function Home() {
 
               {showLogoOnModal ? (
                 <div className="absolute inset-0 overflow-hidden rounded-[16px]">
-                  <img
+                  {/* <img
                     ref={posterRef}
                     src={getCardPoster(cardType)}
                     className="block w-full h-full object-cover"
                     alt=""
-                  />
+                  /> */}
                 </div>
               ) : (
                 <video
